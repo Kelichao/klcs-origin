c0a88b35acf94e61ea8a1c70586fbdb25e07d913
kelichao <961904564@qq.com>
2017-02-27 15:08:04 +0800

    添加了新的update

--- kit.js
+++ kit.js
@@ -4,6 +4,7 @@
  * @update: 2016-11-19
  * @update: 2016-12-28 / 增加了埋点文件以及调用客户端接口文件的方法
  * @update: 2017-01-04 / 增加了MVC架构
+ * @update: 2017-02-25 / 开始增加移动端函数
  * @https://github.com/Kelichao/kit.js
  */
 
