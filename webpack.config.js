046d78d4fea3154dcd7b46a20633d1dddf8340cb
柯立超 <ke.lichao@geely.com>
2017-07-10 17:15:39 +0800

    将代码工程化

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var WebpackDevServer = require("webpack-dev-server");
-var TARGET = process.env.npm_lifecycle_event;
-var path = require("path");
-console.log(TARGET)
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');//帮助打开浏览器
+var TARGET = process.env.npm_lifecycle_event;// 命令名称
+var port = process.env.npm_package_config_port;
+var devtool = "eval-source-map";
 
+if (TARGET === "product") {
+    devtool = "";
+} 
 
 module.exports = {
-    devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
+    devtool: devtool,// 配置生成Source Maps，选择合适的选项
     entry: { bbb: "./src/js/kit.js" },
     output: {
         path: __dirname + "/assets",
@@ -26,8 +30,12 @@ module.exports = {
             compress: {
                 warnings: false
             }
-        })
+        }),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:' + port
+        }),
     ],
+    // 自带热加载
     devServer: {
         contentBase: __dirname + "/assets/index.html",//本地服务器所加载的页面所在的目录
         historyApiFallback: true,//不跳转
