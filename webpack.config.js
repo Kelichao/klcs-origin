e4306972254403fc6d9b4f2b22cf3bf51b267a76
柯立超 <ke.lichao@geely.com>
2017-07-10 16:37:53 +0800

    添加部分有用文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var WebpackDevServer = require("webpack-dev-server");
+var TARGET = process.env.npm_lifecycle_event;
+var path = require("path");
+console.log(TARGET)
+
 
 module.exports = {
+    devtool: 'eval-source-map',//配置生成Source Maps，选择合适的选项
     entry: { bbb: "./src/js/kit.js" },
     output: {
         path: __dirname + "/assets",
@@ -13,11 +19,20 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: __dirname + "/src/enter.html"
+        }),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
         })
-    ]
+    ],
+    devServer: {
+        contentBase: __dirname + "/assets/index.html",//本地服务器所加载的页面所在的目录
+        historyApiFallback: true,//不跳转
+        inline: true//实时刷新
+    }
 };
+
+
